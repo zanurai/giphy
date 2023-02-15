e2428e56706bc77c522a609b9a4f7243cd627ad3
Janaki <zanuthulung@gmail.com>
2023-02-15 13:35:06 +0545

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">Navbar</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,8 +12,8 @@ function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             {/*<li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-</li>*/}
+                               <a className="nav-link active" aria-current="page" href="#">Home</a>
+                             </li>*/}
 
                             <li className="nav-item"><a className="nav-link" href="#">general</a></li>
                             <li className="nav-item"><a className="nav-link" href="#">business</a></li>
